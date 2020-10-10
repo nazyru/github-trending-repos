2fd9e68b2816ba2cc6c00062e441e7e27b34bb81
Nazir Bunu <nazir@Nazirs-MacBook-Pro.local>
2020-10-11 00:06:14 +0100

    Initial project setup

---
+++ src/main/java/com/nazir/githubtrendingrepos/GithubTrendingReposApplication.java
@@ -0,0 +1,13 @@
+package com.nazir.githubtrendingrepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTrendingReposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTrendingReposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nazir/githubtrendingrepos/GithubTrendingReposApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazir.githubtrendingrepos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubTrendingReposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
